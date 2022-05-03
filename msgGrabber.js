d34288c6d4cd00fae848875c3380fba663ce4c28
Admiral Jezza <zigg.js@protonmail.com>
2022-05-03 04:59:23 +0100

    New line

--- msgGrabber.js
+++ msgGrabber.js
@@ -1,13 +1,14 @@
 const msgArray = ["Go to the wikipedia page for the USS North Carolina (BB-55), look for the second citation [45] under the Battleship Memorial secion!",
     "¯＼_(ツ)_/¯",
     "On the internet all women are men and all kids are FBI agents",
-    "people who play vrchat walk around in big tiddy mommy milker avatars and then wonder why they get gender dysphoria",
+    "people who play vrchat walk around in big tiddy mommy milker avatars and then wonder why they get gender dysphoria -gangstertoppen",
     "youtube.com/watch?v=iik25wqIuFo",
     "Cool it with the anti-semitic remarks.",
     "+52° 28′ 47.14″, +62° 11′ 8.38",
     "あなたはバカです",
     "Thank you Alec, very cool",
-    "Maybe you should try getting a job"
+    "Maybe you should try getting a job",
+    "She's moving the camera around like a crack addicted rattlesnake with attention deficit disorder -TheMightyJingles"
 ];
 
 let x = Math.floor(Math.random() * msgArray.length);
