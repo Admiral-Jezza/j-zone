80c53741a103fe632460c6c64f9052d88767cab9
Admiral-Jezza <zigg.js@protonmail.com>
2022-09-15 13:02:15 +0100

    Images

--- imgGrabber.js
+++ imgGrabber.js
@@ -1,11 +1,17 @@
-const imgCount = 2;
+const imgArray = [
+    "https://files.catbox.moe/wgyxdn.png",
+    "https://files.catbox.moe/p9nw5f.png",
+    "https://files.catbox.moe/87xrfe.png"
+]
 
-let y = Math.floor(Math.random() * imgCount);
+
+let y = Math.floor(Math.random() * imgArray.length);
 
 
 function imgGrab() {
     var img = document.getElementById("mainImage");
-    img.innerHTML = "<img src=\"https://files.catbox.moe/wgyxdn.png\">"
+    var imgLink = "<img src=\"" + imgArray[y] +  "\">";
+    img.innerHTML = imgLink;
 }
 
 imgGrab();
